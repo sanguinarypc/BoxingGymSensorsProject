295154d6f7e7671b82c8d511f954e2770f901730
Nick Dimitrakarakos <nm_dim@otenet.gr>
2025-05-08 20:38:19 +0300

    fix screen paths

--- box_sensors/lib/widgets/my_app.dart
+++ box_sensors/lib/widgets/my_app.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:box_sensors/services/providers.dart';
-import 'package:box_sensors/screens_widgets/connect_home_screen.dart';
+import 'package:box_sensors/screens/connect_home_screen.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class MyApp extends ConsumerWidget {
