3d8c8ab00c2a834474f20725837cefce93fe62b4
Nick Dimitrakarakos <nm_dim@otenet.gr>
2026-01-26 01:29:13 +0200

    Update device_config.dart

--- box_sensors/lib/utils/device_config.dart
+++ box_sensors/lib/utils/device_config.dart
@@ -13,6 +13,12 @@ class DeviceConfig {
 
   static const String webServerUrl =
       "https://boxing-dashboard.ndimitrakarakos.gr/api/data";
+
+  //   static const String webServerUrl = String.fromEnvironment(
+  // 'WEB_SERVER_URL',
+  // defaultValue: 'https://boxing-dashboard.ndimitrakarakos.gr/api/data',
+  // );
+  // flutter run --dart-define=WEB_SERVER_URL=http://192.168.1.3:3000/api/data
   // static const String webServerUrl = "https://ndim.codecraft.gr/receiver.php";
   // static const String webServerUrl = "https://ndim.codecraft.gr/api/data";
   // static const String webServerUrl = "http://192.168.1.3:3000/api/data";
