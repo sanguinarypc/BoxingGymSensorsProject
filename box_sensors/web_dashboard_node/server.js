c80f45f853498e86dcc9e7ab76e9d47bdac1b08d
Nick Dimitrakarakos <nm_dim@otenet.gr>
2026-01-26 00:45:16 +0200

    Update server.js

--- box_sensors/web_dashboard_node/server.js
+++ box_sensors/web_dashboard_node/server.js
@@ -151,11 +151,6 @@ app.post('/api/data', (req, res) => {
     res.json({ success: true });
 });
 
-// Start Server
-//app.listen(PORT, () => {
-//    console.log(`Server running at http://localhost:${PORT}`);
-//});
-
 // Start Server (bind ONLY to localhost)
 app.listen(PORT, '127.0.0.1', () => {
     console.log(`Server running at http://127.0.0.1:${PORT}`);
